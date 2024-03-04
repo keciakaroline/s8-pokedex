fd6d05e44848ffa372a51b96f886804d045e23ae
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-04 13:49:40 -0300

    feat: Add PokemonDetails.jsx

---
+++ src/pages/PokemonDetails.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navigation from "../components/Navigation/Navigation";
+
+export default function PokemonDetails() {
+  return (
+    <>
+      <Navigation navItems={[{ url: "/:id", label: "Pokemon Details" }]} />
+      <div className="content">
+        <Outlet />
+      </div>
+    </>
+  );
+}
