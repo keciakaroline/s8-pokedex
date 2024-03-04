ad170ab92e7af95f09b20735fae3f02dcafd49f4
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-04 14:36:14 -0300

    refactor: getPokemons function to handle API response and errors

--- src/components/api/pokemonApi.js
+++ src/components/api/pokemonApi.js
@@ -1,7 +1,16 @@
 import axiosClient from "./axiosClient";
 
 export function getPokemons() {
-  return axiosClient.get("/pokemon");
+  return axiosClient
+    .get("/pokemon")
+    .then((response) => {
+      console.log("response.data.results", response.data.results);
+      return response.data.results;
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
 }
 
 export function getPokemonById(id) {
