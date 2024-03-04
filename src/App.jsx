fe475fd667e124a7af90ce7478d1e392cf8f187f
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-04 14:36:03 -0300

    feat: App component to use react-query for data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root from "./pages/Pokedex";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PokemonDetails from "./pages/PokemonDetails";
 import Pokedex from "./pages/Pokedex";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Pokedex />,
-    // children: [
-    //   {
-    //      path: "/",
-    //      element: <Statistics />,
-    //   },
   },
   {
     path: "/:id",
     element: <PokemonDetails />,
     // children: [
     //    { path: "player", element: <CreatePlayerForm /> },
-    //    { path: "team", element: <CreateTeamForm /> },
-    //    { path: "match", element: <CreateMatchForm /> },
     // ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
