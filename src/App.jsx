0d38560948f78182bbe16c6f1946b8d3f7cdf157
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-04 13:50:14 -0300

    feat: Update routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root from "./pages/Root";
-import Create from "./pages/Create";
+import Root from "./pages/Pokedex";
+import PokemonDetails from "./pages/PokemonDetails";
+import Pokedex from "./pages/Pokedex";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-    children: [
-      {
-        // path: "/",
-        // element: <Statistics />,
-      },
-      {
-        path: "create",
-        element: <Create />,
-        children: [
-          // { path: "player", element: <CreatePlayerForm /> },
-          // { path: "team", element: <CreateTeamForm /> },
-          // { path: "match", element: <CreateMatchForm /> },
-        ],
-      },
-    ],
+    element: <Pokedex />,
+    // children: [
+    //   {
+    //      path: "/",
+    //      element: <Statistics />,
+    //   },
+  },
+  {
+    path: "/:id",
+    element: <PokemonDetails />,
+    // children: [
+    //    { path: "player", element: <CreatePlayerForm /> },
+    //    { path: "team", element: <CreateTeamForm /> },
+    //    { path: "match", element: <CreateMatchForm /> },
+    // ],
   },
 ]);
 
