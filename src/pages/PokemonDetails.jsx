5e2fadd2bcfd9e660b6a423450c5a091dccc53ac
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-05 18:06:54 -0300

    refactor: Update PokemonDetails component

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -4,10 +4,8 @@ import Navigation from "../components/Navigation/Navigation";
 export default function PokemonDetails() {
   return (
     <>
-      <Navigation navItems={[{ url: "/:id", label: "Pokemon Details" }]} />
-      <div className="content">
-        <Outlet />
-      </div>
+      {/* <Navigation navItems={[{ url: "/:id", label: "Pokemon Details" }]} /> */}
+      <div className="content">ALGO AQUI</div>
     </>
   );
 }
