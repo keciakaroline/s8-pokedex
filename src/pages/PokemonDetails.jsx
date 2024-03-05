fae944f5d0cb4718ad797fc466cdedefd73f1e73
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-05 19:02:28 -0300

    feat: Add hooks for fetching Pokemon species and details

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { usePokemonSpecies } from "../hooks/usePokemonSpecies";
+import { usePokemonByName } from "../hooks/usePokemonByName";
 
 export default function PokemonDetails() {
   const { id } = useParams();
   const [pokemonDetail, setPokemonDetail] = useState(null);
+
+  const { pokemon, isLoading, isError, error } = usePokemonByName(id);
+  const { pokemonSpecie, isLoadingSpecie, isErrorSpecie, errorSpecie } =
+    usePokemonSpecies(id);
+
+  if (isError) return <p>An error has occurred: {error.message}</p>;
+
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <>
