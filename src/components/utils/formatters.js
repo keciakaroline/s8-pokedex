4c01596d4f6ce4d89cfc7eac27659fc193563a7d
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-06 13:02:01 -0300

    feat: Add capitalizeName and turnDecimal functions to formatters.js

--- src/components/utils/formatters.js
+++ src/components/utils/formatters.js
@@ -3,3 +3,11 @@ export function cleanFlavorText(flavorText) {
     ? flavorText.replace(/\\f|\\n/g, " ").replace(/POKéMON/g, "Pokémon")
     : "";
 }
+
+export const capitalizeName = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+};
+
+export const turnDecimal = (value) => {
+  return (value / 10).toFixed(1);
+};
