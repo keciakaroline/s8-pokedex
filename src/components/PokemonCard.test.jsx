f832e590767d38dcfab3ac406a885a81b06a3197
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-08 10:44:15 -0300

    test: Add formattedId to PokemonCard test and render link for next page

--- src/components/PokemonCard.test.jsx
+++ src/components/PokemonCard.test.jsx
@@ -2,9 +2,26 @@ import PokemonCard from "./PokemonCard";
 import { customRender } from "../../test/support/customRender";
 import { describe, expect, it } from "vitest";
 import { screen } from "@testing-library/react";
+import { formatId } from "./utils/formatters";
 
 describe("PokemonCard", () => {
   it("renders name, id and sprite", () => {
+    const pokemonName = "bulbasaur";
+    const pokemonId = 1;
+    const formattedId = formatId(pokemonId);
+
+    customRender(
+      <PokemonCard
+        name={pokemonName}
+        id={pokemonId}
+      />
+    );
+
+    expect.soft(screen.getByText(pokemonName)).toBeInTheDocument();
+    expect.soft(screen.getByText(formattedId)).toBeInTheDocument();
+    expect.soft(screen.getByAltText(pokemonName)).toBeInTheDocument();
+  });
+  it("renders the link for the next page", () => {
     const pokemonName = "bulbasaur";
     const pokemonId = 1;
 
@@ -15,9 +32,9 @@ describe("PokemonCard", () => {
       />
     );
 
-    expect.soft(screen.getByText(pokemonName)).toBeInTheDocument();
-    expect.soft(screen.getByText("#001")).toBeInTheDocument();
-    expect.soft(screen.getByAltText(pokemonName)).toBeInTheDocument();
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      `/pokemons/${pokemonName}/`
+    );
   });
-  it("renders the link for the next page", () => {});
 });
