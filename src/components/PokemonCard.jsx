18eeac025f0ca0787fba2af44468c0af46f065df
kecia.karoline <kecia.karoline@codeminer42.com>
2024-03-08 10:36:40 -0300

    feat: Add PokemonCard component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import { formatId } from "../components/utils/formatters";
+
+export default function PokemonCard({ name, id, sprites }) {
+  const formattedId = formatId(id);
+
+  return (
+    <li className="pokemonCard">
+      <Link
+        to={`/pokemons/${name}/`}
+        className="pokemonCard"
+      >
+        <div className="pokemonCard_id">{formattedId}</div>
+        <div className="pokemonCard_name">{name}</div>
+        <img
+          className="pokemonCard_img"
+          src={sprites}
+          alt={name}
+        />
+      </Link>
+    </li>
+  );
+}
